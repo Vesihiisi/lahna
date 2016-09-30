69df69ec0e276078a37261fe4ca088e81719ca0f
Vesihiisi <alicia@fagerving.se>
2016-09-30 08:51:28 +0200

    reading from json works

--- lahna.py
+++ lahna.py
@@ -4,7 +4,8 @@ import csv
 import codecs
 import mwclient
 from operator import itemgetter
-
+import json
+from pprint import pprint
 
 def get_iwlist(page):
     languages = page.langlinks()
@@ -77,7 +78,10 @@ def processall(data):
     outputlist = iwprocess(data)
     sorteddata = sort(outputlist)
     formattedoutput = wikiformat(sorteddata)
-    myfile = saveoutput(formattedoutput, generateFileName(languagecode, targetlanguage, categoryname))
+    if (args.file):
+        myfile = saveoutput(formattedoutput, "test.txt")
+    else:
+        myfile = saveoutput(formattedoutput, generateFileName(languagecode, targetlanguage, categoryname))
     return myfile
 
 def generateFileName(source, target, category):
@@ -87,20 +91,28 @@ def generateFileName(source, target, category):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-c", "--category", required=True)
+        "-c", "--category", required=False)
     parser.add_argument("-s", "--sourcelanguage", default='en', required=True)
     parser.add_argument("-t", "--targetlanguage", default='sv', required=True)
     parser.add_argument("-d", "--depth", default='0', required=False)
-    args = parser.parse_args()
     parser.add_argument("-f", "--file", default=None)
+    args = parser.parse_args()
     filename = args.file
     languagecode = args.sourcelanguage
     categoryname = args.category
     targetlanguage = args.targetlanguage
     site = mwclient.Site(('https', languagecode + '.wikipedia.org'))
     if args.file:
-        data = listFromFile(filename)
+        with open(filename) as data_file:    
+            data = json.load(data_file)
+            itemList = (data["*"][0]["a"]["*"])
+            titleList = []
+            for item in itemList:
+                print(item["title"])
+                titleList.append(item["title"])
+        myfile = processall(titleList)
+        #data = listFromFile(filename)
     else:
         category = site.Categories[categoryname]
         data = newlistpages(category, int(args.depth))
-    myfile = processall(data)
+    #myfile = processall(data)
